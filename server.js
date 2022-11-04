e45d8fbc47d22d9c417d97690d2ace85412d27ea
EllisJLC <jia.liang.chen@ryerson.ca>
2022-11-03 21:57:44 -0400

    fixed dotenv require

--- server.js
+++ server.js
@@ -8,13 +8,17 @@ const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers }); // run handlebars engine
+
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: "secret secret secret",
   cookie: {
     maxAge: 300000,
     httpOnly: true,
@@ -30,15 +34,16 @@ const sess = {
 
 app.use(session(sess));
 
+// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars'); // configure express view engine
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public'))); // set directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`));
+  app.listen(PORT, () => console.log('Now listening'));
 });
