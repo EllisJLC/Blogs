1928ef030d9f83387399be679f8ccdf52ab7e58f
EllisJLC <jia.liang.chen@ryerson.ca>
2022-11-03 22:13:26 -0400

    dotenv

--- server.js
+++ server.js
@@ -8,12 +8,9 @@ const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// require('dotenv').config();
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
 const sess = {
